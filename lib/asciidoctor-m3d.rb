c5746037a1952ec693e718f552b028f6f452971c
Nick Nicholas <opoudjis@optusnet.com.au>
2018-05-25 20:57:11 +1000

    word output

--- lib/asciidoctor-m3d.rb
+++ lib/asciidoctor-m3d.rb
@@ -1,4 +1,5 @@
 require "asciidoctor" unless defined? Asciidoctor::Converter
 require_relative "asciidoctor/m3d/converter"
 require_relative "isodoc/m3d/m3dconvert"
+require_relative "isodoc/m3d/m3dwordconvert"
 require_relative "asciidoctor/m3d/version"
