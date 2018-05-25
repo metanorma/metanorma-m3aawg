4fe21df8fa36900b014c34503dddd928115c1bae
Nick Nicholas <opoudjis@optusnet.com.au>
2018-05-25 20:26:34 +1000

    gem setup

--- lib/asciidoctor-m3d.rb
+++ lib/asciidoctor-m3d.rb
@@ -1,4 +1,4 @@
 require "asciidoctor" unless defined? Asciidoctor::Converter
 require_relative "asciidoctor/m3d/converter"
-require_relative "asciidoctor/m3d/m3dconvert"
+require_relative "isodoc/m3d/m3dconvert"
 require_relative "asciidoctor/m3d/version"
