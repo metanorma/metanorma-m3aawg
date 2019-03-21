b5fb2015d717ce1cdbb2dea605557d8791b7065c
Nick Nicholas <opoudjis@optusnet.com.au>
2019-03-21 23:36:45 +1100

    conditional validation; https://github.com/metanorma/metanorma-standoc/issues/44

--- lib/asciidoctor/m3d/converter.rb
+++ lib/asciidoctor/m3d/converter.rb
@@ -87,7 +87,7 @@ module Asciidoctor
         result << "</m3d-standard>"
         result = textcleanup(result)
         ret1 = cleanup(Nokogiri::XML(result))
-        validate(ret1)
+        validate(ret1) unless @novalid
         ret1.root.add_namespace(nil, M3D_NAMESPACE)
         ret1
       end
