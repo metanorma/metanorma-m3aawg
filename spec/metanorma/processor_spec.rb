8aa8b1df094d5620303fb119d455a47250a1b805
Nick Nicholas <opoudjis@gmail.com>
2021-11-30 01:25:58 +1100

    version bump

--- spec/metanorma/processor_spec.rb
+++ spec/metanorma/processor_spec.rb
@@ -3,7 +3,6 @@ require "metanorma"
 require "fileutils"
 
 RSpec.describe Metanorma::M3AAWG::Processor do
-
   registry = Metanorma::Registry.instance
   registry.register(Metanorma::M3AAWG::Processor)
   processor = registry.find_processor(:m3aawg)
@@ -14,7 +13,7 @@ RSpec.describe Metanorma::M3AAWG::Processor do
 
   it "registers output formats against metanorma" do
     expect(processor.output_formats.sort.to_s).to be_equivalent_to <<~"OUTPUT"
-    [[:doc, "doc"], [:html, "html"], [:pdf, "pdf"], [:presentation, "presentation.xml"], [:rxl, "rxl"], [:xml, "xml"]]
+      [[:doc, "doc"], [:html, "html"], [:pdf, "pdf"], [:presentation, "presentation.xml"], [:rxl, "rxl"], [:xml, "xml"]]
     OUTPUT
   end
 
@@ -23,38 +22,43 @@ RSpec.describe Metanorma::M3AAWG::Processor do
   end
 
   it "generates IsoDoc XML from a blank document" do
-    expect(xmlpp(strip_guid(processor.input_to_isodoc(<<~"INPUT", nil)))).to be_equivalent_to xmlpp(<<~"OUTPUT")
-    #{ASCIIDOC_BLANK_HDR}
+    input = <<~INPUT
+      #{ASCIIDOC_BLANK_HDR}
     INPUT
-    #{BLANK_HDR}
-<sections/>
-</m3d-standard>
+    output = <<~OUTPUT
+          #{BLANK_HDR}
+      <sections/>
+      </m3d-standard>
     OUTPUT
+    expect(xmlpp(strip_guid(processor
+      .input_to_isodoc(input, nil))))
+      .to be_equivalent_to xmlpp(output)
   end
 
   it "generates HTML from IsoDoc XML" do
     FileUtils.rm_f "test.xml"
     processor.output(<<~"INPUT", "test.xml", "test.html", :html)
-               <m3d-standard xmlns="http://riboseinc.com/isoxml">
-       <sections>
-       <terms id="H" obligation="normative"><title>1.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
-         <term id="J">
-         <name>1.1.</name>
-         <preferred>Term2</preferred>
-       </term>
-        </terms>
-        </sections>
-        </m3d-standard>
+              <m3d-standard xmlns="http://riboseinc.com/isoxml">
+      <sections>
+      <terms id="H" obligation="normative"><title>1.<tab/>Terms, Definitions, Symbols and Abbreviated Terms</title>
+        <term id="J">
+        <name>1.1.</name>
+        <preferred>Term2</preferred>
+      </term>
+       </terms>
+       </sections>
+       </m3d-standard>
     INPUT
-    expect(xmlpp(File.read("test.html", encoding: "utf-8").gsub(%r{^.*<main}m, "<main").gsub(%r{</main>.*}m, "</main>"))).to be_equivalent_to xmlpp(<<~"OUTPUT")
-           <main class="main-section"><button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-             <p class="zzSTDTitle1"></p>
-             <div id="H"><h1 id="toc0">1.&#xA0; Terms, Definitions, Symbols and Abbreviated Terms</h1>
-       <h2 class="TermNum" id="J">1.1.&#xA0;<p class="Terms" style="text-align:left;">Term2</p></h2>
-
-       </div>
-           </main>
-    OUTPUT
+    expect(xmlpp(File.read("test.html", encoding: "utf-8")
+      .gsub(%r{^.*<main}m, "<main")
+      .gsub(%r{</main>.*}m, "</main>")))
+      .to be_equivalent_to xmlpp(<<~"OUTPUT")
+            <main class="main-section"><button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+              <p class="zzSTDTitle1"></p>
+              <div id="H"><h1 id="toc0">1.&#xA0; Terms, Definitions, Symbols and Abbreviated Terms</h1>
+            <p class='Terms' style='text-align:left;' id='J'><strong>1.1.</strong>&#xA0;Term2</p>
+        </div>
+            </main>
+      OUTPUT
   end
-
 end
