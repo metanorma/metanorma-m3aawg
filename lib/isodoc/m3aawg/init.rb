cefa7329e2f762a8b0e14245f1331c1103fb7a32
Nick Nicholas <opoudjis@gmail.com>
2022-05-09 23:27:48 +1000

    version bump

--- lib/isodoc/m3aawg/init.rb
+++ lib/isodoc/m3aawg/init.rb
@@ -10,13 +10,13 @@ module IsoDoc
         @meta = Metadata.new(lang, script, labels)
       end
 
-      def xref_init(lang, script, klass, labels, options)
+      def xref_init(lang, script, _klass, labels, options)
         html = HtmlConvert.new(language: lang, script: script)
         @xrefs = Xref.new(lang, script, html, labels, options)
       end
 
       def i18n_init(lang, script, i18nyaml = nil)
-        @i18n = I18n.new(lang, script, i18nyaml || @i18nyaml)
+        @i18n = I18n.new(lang, script, i18nyaml: i18nyaml || @i18nyaml)
       end
     end
   end
